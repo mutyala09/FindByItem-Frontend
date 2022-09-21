afa57087b5c0551da73080a49e1eef739fe74ee3
mutyala09 <79491003+mutyala09@users.noreply.github.com>
2022-09-21 13:50:21 -0400

    Add files via upload
    BackEnd Application (Springboot)

---
+++ viewitems/src/main/java/com/viewbyitems/viewitems/ViewitemsApplication.java
@@ -0,0 +1,13 @@
+package com.viewbyitems.viewitems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewitemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewitemsApplication.class, args);
+	}
+
+}
